45002456bb8416a08b3c2ed5cfb7fdfefe2ec38a
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2020-06-17 20:44:30 -0300

    create base structure for ceos fracture service

---
+++ db/errors/fracture.js
@@ -0,0 +1,5 @@
+module.exports = {
+  RECORD_NUMBER_REQUIRED: 'you must inform a record number to register a fracture',
+  TRAUMA_KIND_REQUIRED: 'Trauma kind is required on associated injury',
+  PATIENT_REQUIRED: 'You cannot register a fracture without registering a patient!'
+};
---
+++ db/models/index.js
@@ -0,0 +1,5 @@
+const ExposedFracture = require('./main');
+
+module.exports = {
+  ExposedFracture,
+};
