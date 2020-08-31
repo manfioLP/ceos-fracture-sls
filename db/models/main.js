65390c3adbd9c664f26e0bc0f953603e049acbc7
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2020-08-31 19:29:24 -0300

    change enums type and fix some

--- db/models/main.js
+++ db/models/main.js
@@ -5,14 +5,14 @@ const { fracture } = require('../errors');
 const ExposedFractureSchema = new mongoose.Schema({
   gustillo: {
     type: String,
-    enum: ['1', '2', '3', 'N/A'],
-    default: 'N/A',
+    enum: ['1', '2', '3', 'N/I'],
+    default: 'N/I',
     trim: true
   },
   ao: {
     type: String,
-    enum: ['A', 'B', 'C', 'N/A'],
-    default: 'N/A',
+    enum: ['A', 'B', 'C', 'N/I'],
+    default: 'N/I',
     trim: true
   },
   recordNumber: {
@@ -22,42 +22,37 @@ const ExposedFractureSchema = new mongoose.Schema({
   },
   description: {
     type: String,
-    default: 'N/A',
+    default: 'N/I',
     trim: true
   },
   limb: {
     type: String,
-    default: 'N/A',
+    default: 'N/I',
     trim: true
   },
   bone: {
     type: String,
-    default: 'N/A',
+    default: 'N/I',
     trim: true
   },
   region: {
     type: String,
-    default: 'N/A',
+    default: 'N/I',
     trim: true
   },
   mechanism: {
     type: String,
-    default: 'N/A',
+    default: 'N/I',
     trim: true
   },
-  associatedTraumaInjury: [{
-    kind: {
-      type: String,
-      required: [true, fracture.TRAUMA_KIND_REQUIRED],
-      trim: true
-    },
-    description: {
-      type: String,
-    }
-  }],
+  associatedFractureTraumaInjury: {
+    type: String,
+    default: 'N/I',
+    trim: true
+  },
   firstSurgicalApproach: {
     type: String,
-    default: 'N/A',
+    default: 'N/I',
     trim: true
   },
   amputation: {
